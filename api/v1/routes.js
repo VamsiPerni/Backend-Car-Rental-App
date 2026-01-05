1a6d6d4c14239a2c4755f3fda5578117719fdd10
VamsiPerni <mohanperni777@gmail.com>
2026-01-05 23:13:48 +0530

    refactor: removing middleware and adding middleware
    adding the separate routes and making the partial website accessible to users who are not logged-in too

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -9,11 +9,11 @@ const apiRouter = express.Router();
 
 apiRouter.use("/auth", authRouter);
 
-apiRouter.use(userAuthenticationMiddleware); // authentication
+// apiRouter.use(userAuthenticationMiddleware); // authentication  // this line will protect the below written urls as it will check everytime whether user is logggedIn or not , if user is loggedIn then only it will allow to route to the below url's or else we must login-first , instead of that we are showing some components without loggin-in. So, we need to do pass this as argument when we are calling/mentioning the route as below
 // all the routes below this middleware are now (protected APIs)
 
-apiRouter.use("/users", usersRouter);
+apiRouter.use("/users", userAuthenticationMiddleware, usersRouter);
 apiRouter.use("/products", productRouter);
-apiRouter.use("/ai", llmRouter);
+apiRouter.use("/ai", userAuthenticationMiddleware, llmRouter);
 
 module.exports = { apiRouter };
